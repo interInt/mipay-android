ad24fdd3862dfd4d701482beebecddf801e6289a
danleyb2interintel <brian.nyaundi@interintel.co.ke>
2017-10-20 11:50:19 +0300

    add sale and order apis

--- mipay/src/main/java/ke/co/interintel/mipay/Mipay.java
+++ mipay/src/main/java/ke/co/interintel/mipay/Mipay.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.text.TextUtils;
 
 import static ke.co.interintel.mipay.Constants.MIPAY_APP;
 import static ke.co.interintel.mipay.Constants.MIPAY_HOST;
@@ -28,6 +29,52 @@ public class Mipay {
     public static void checkout(Activity activity, String reference) {
         Intent i;
         String httpUrl = "https://" + MIPAY_HOST + "/checkout/?reference=" + reference;
+
+        if (isAppInstalled(activity, MIPAY_APP)) {
+            i = new Intent();
+            i.setClassName(MIPAY_APP, MIPAY_APP + ".SplashActivity");
+        } else {
+            i = new Intent(activity, WebViewActivity.class);
+        }
+
+        i.setData(Uri.parse(httpUrl.toString()));
+
+
+        activity.startActivityForResult(i, REQUEST_CODE_MIPAY_PAYMENT);
+
+    }
+
+    private static String[] toStringA(int[] intArray) {
+        String strArray[] = new String[intArray.length];
+
+        for (int i = 0; i < intArray.length; i++)
+            strArray[i] = String.valueOf(intArray[i]);
+
+        return strArray;
+    }
+
+    public static void order(Activity activity, int[] cartItems) {
+        String cartItemsS = TextUtils.join(", ", toStringA(cartItems));
+        Intent i;
+        String httpUrl = "https://" + MIPAY_HOST + "/order/?cart_items=" + cartItemsS;
+
+        if (isAppInstalled(activity, MIPAY_APP)) {
+            i = new Intent();
+            i.setClassName(MIPAY_APP, MIPAY_APP + ".SplashActivity");
+        } else {
+            i = new Intent(activity, WebViewActivity.class);
+        }
+
+        i.setData(Uri.parse(httpUrl.toString()));
+
+
+        activity.startActivityForResult(i, REQUEST_CODE_MIPAY_PAYMENT);
+
+    }
+
+    public static void sale(Activity activity, int productItemID, int quantity) {
+        Intent i;
+        String httpUrl = "https://" + MIPAY_HOST + "/order/?product_item_id=" + String.valueOf(productItemID) + "\"&quantity=" + String.valueOf(quantity);
 
         if (isAppInstalled(activity, MIPAY_APP)) {
             i = new Intent();
