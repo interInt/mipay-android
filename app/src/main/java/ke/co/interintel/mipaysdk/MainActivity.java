f22026ed0e36464e57c19a51ea30c1f7be8db866
Elvin Ega <proega3@gmail.com>
2017-10-23 10:40:08 +0300

    Add toolbar on webview with progressbar

--- app/src/main/java/ke/co/interintel/mipaysdk/MainActivity.java
+++ app/src/main/java/ke/co/interintel/mipaysdk/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -12,13 +13,18 @@ import org.json.JSONObject;
 
 import ke.co.interintel.mipay.Mipay;
 
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
--- mipay/src/main/java/ke/co/interintel/mipay/WebViewActivity.java
+++ mipay/src/main/java/ke/co/interintel/mipay/WebViewActivity.java
@@ -1,8 +1,13 @@
 package ke.co.interintel.mipay;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,23 +15,43 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class WebViewActivity extends Activity implements Animation.AnimationListener {
+public class WebViewActivity extends AppCompatActivity implements Animation.AnimationListener {
 
     // Animation
     Animation animLoad;
     ImageView mipay_loading;
+    //web UI
     WebView webView;
+    ProgressBar progressBar;
+    Toolbar toolbar_webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        //set up the toolbar
+        toolbar_webview = (Toolbar)findViewById(R.id.toolbar_webview);
+        setSupportActionBar(toolbar_webview);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //change back icon
+        if (toolbar_webview != null) {
+            toolbar_webview.setNavigationIcon(R.drawable.ic_clear_black_24dp);
+            toolbar_webview.setTitle(R.string.webview_title);
+
+        }
+        final ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar_webview);
+
+        //Get Data passed through intent
         final Intent sIntent = getIntent();
         String urlString = sIntent.getDataString();
-        mipay_loading = (ImageView)findViewById(R.id.mipay_loading);
+
+
         // load the animation
+        mipay_loading = (ImageView)findViewById(R.id.mipay_loading);
         animLoad = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.rotate);
 
@@ -40,7 +65,6 @@ public class WebViewActivity extends Activity implements Animation.AnimationList
         webView.getSettings().setDatabaseEnabled(true);
 
         // Set WebView client
-        //webView.setWebChromeClient(new WebChromeClient());
         webView.addJavascriptInterface(new WebAppInterface(this), "Android");
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient() {
@@ -48,6 +72,10 @@ public class WebViewActivity extends Activity implements Animation.AnimationList
                 if(newProgress >=100){
                     mipay_loading.setVisibility(View.GONE);
                     mipay_loading.clearAnimation();
+                    toolbar_webview.setTitle(webView.getTitle());
+
+                }else{
+                    progressBar.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -56,18 +84,34 @@ public class WebViewActivity extends Activity implements Animation.AnimationList
 
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
     @Override
-    public void onAnimationStart(Animation animation) {
-        if (animation == animLoad) {
-            Toast.makeText(getApplicationContext(), "Loading Stopped",
-                    Toast.LENGTH_SHORT).show();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getOrder()) {
+            case 0:
+                finish();
+                break;
         }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onAnimationStart(Animation animation) {
+
     }
 
     @Override
     public void onAnimationEnd(Animation animation) {
 
+        if (animation != animLoad) {
+            Toast.makeText(getApplicationContext(), "Loading Stopped",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
